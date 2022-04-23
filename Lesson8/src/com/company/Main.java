74d39f9f6692e516b38f25e0001675d6a85e32af
George <georgukladnik@gmail.com>
2022-04-23 21:20:00 +0300

    HashTable

--- Lesson8/src/com/company/Main.java
+++ Lesson8/src/com/company/Main.java
@@ -2,6 +2,14 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-
+         HashTable<String,Integer> ht= new HashTable<>(10);
+         ht.put("a",5);
+         ht.put("b",8);
+         ht.put("c",4);
+         ht.display();
+         ht.remove("b");
+         int v = ht.find("c");
+         System.out.println(v);
+         ht.display();
     }
 }
